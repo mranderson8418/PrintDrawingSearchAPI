cb8f8c08889eb21a0c9f2dcc501ca3fb71437f1d
MrAnderson8418 <mranderson8418@gmail.com>
2025-05-14 10:31:56 -0400

    commit

--- src/main/java/com/printdrawingsearch/controllers/RegistrationController.java
+++ src/main/java/com/printdrawingsearch/controllers/RegistrationController.java
@@ -43,7 +43,7 @@ public class RegistrationController {
 	 */
 	@PostMapping("/register/user")
 	// @CrossOrigin(origins = "http://127.0.0.1:5501")
-	@CrossOrigin(origins = "https://printsearch-frontend-production.up.railway.app/")
+	@CrossOrigin(origins = "printsearch-frontend.railway.internal")
 	public ResponseEntity<String> createUser(@RequestBody MyUserDto myUserDto) {
 
 		logger.trace("Entered......createUser() ");
