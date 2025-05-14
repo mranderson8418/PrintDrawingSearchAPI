208091a29e8290d166f0bfda7cf5beb82ebd60b3
MrAnderson8418 <mranderson8418@gmail.com>
2025-05-14 10:22:22 -0400

    commit

--- src/main/java/com/printdrawingsearch/security/SecurityConfiguration.java
+++ src/main/java/com/printdrawingsearch/security/SecurityConfiguration.java
@@ -74,7 +74,7 @@ public class SecurityConfiguration {
 		CorsConfiguration configuration = new CorsConfiguration();
 		// Allow requests from these origins
 		// Example origins. replace with your origins
-		configuration.setAllowedOrigins(Arrays.asList("http://127.0.0.1:5500", "http://127.0.0.1:5501", "http://localhost:3000","https://printsearch-frontend-production.up.railway.app"));
+		configuration.setAllowedOrigins(Arrays.asList("http://127.0.0.1:5500", "http://127.0.0.1:5501", "http://localhost:3000","https://printsearch-frontend-production.up.railway.app","printsearch-frontend.railway.internal"));
 		// Allow these HTTP methods
 		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 		// Allow these headers
